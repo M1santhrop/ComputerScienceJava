03ec5a5daafbcd1d3814d3bc8e0b3365defcc283
DmitryTimofeev <dmitryTimofeev@coherentsolutions.com>
2023-02-28 14:00:12 +0100

    CSJ-7: added counter of operations for Hanoi task

--- src/chapter1/Hanoi.java
+++ src/chapter1/Hanoi.java
@@ -9,20 +9,14 @@ public class Hanoi {
     };
     public final Deque<Integer> towerB = new ArrayDeque<>();
     public final Deque<Integer> towerC = new ArrayDeque<>();
+    public int countOfOperations;
 
     public Hanoi(int discs) {
+        countOfOperations = 0;
         numDiscs = discs;
         for (int i = 1; i <= numDiscs; i++) {
             towerA.push(i);
         }
-    }
-
-    public static void main(String[] args) {
-        Hanoi hanoi = new Hanoi(20);
-        hanoi.solve();
-        System.out.println(hanoi.towerA.size());
-        System.out.println(hanoi.towerB.size());
-        System.out.println(hanoi.towerC.size());
     }
 
     public void solve() {
@@ -31,11 +25,21 @@ public class Hanoi {
 
     private void move(Deque<Integer> begin, Deque<Integer> end, Deque<Integer> temp, int n) {
         if (n == 1) {
+            countOfOperations++;
             end.push(begin.pop());
         } else {
             move(begin, temp, end, n - 1);
             move(begin, end, temp, 1);
             move(temp, end, begin, n - 1);
         }
+    }
+
+    public static void main(String[] args) {
+        Hanoi hanoi = new Hanoi(5);
+        hanoi.solve();
+        System.out.println(hanoi.towerA.size());
+        System.out.println(hanoi.towerB.size());
+        System.out.println(hanoi.towerC.size());
+        System.out.println(hanoi.countOfOperations);
     }
 }
